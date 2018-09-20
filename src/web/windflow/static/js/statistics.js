ef10fb0db9750114d8445066ba82dc28ab38fcc0
River Chu <churiver@protonmail.com>
2018-09-20 08:48:32 +0800

    增加js css images基础文件

---
+++ src/web/windflow/static/js/statistics.js
@@ -0,0 +1,107 @@
+var vulnChart = echarts.init(document.getElementById('vuln-chart'));
+var assetsChart = echarts.init(document.getElementById('assets-chart'));
+
+var vulnOption = {
+    title : {
+        text: '漏洞分类统计',
+        subtext: '',
+        x:'center'
+    },
+    tooltip : {
+        trigger: 'item',
+        formatter: "{a} <br/>{b} : {c} ({d}%)"
+    },
+    legend: {
+        orient: 'vertical',
+        left: 'left',
+        data: ['弱口令','命令执行','代码执行','web shell','提权漏洞']
+    },
+    series : [
+        {
+            name: '访问来源',
+            type: 'pie',
+            radius : '55%',
+            center: ['50%', '60%'],
+            data:[
+                {value:335, name:'弱口令'},
+                {value:310, name:'命令执行'},
+                {value:234, name:'代码执行'},
+                {value:135, name:'web shell'},
+                {value:1548, name:'提权漏洞'}
+            ],
+            itemStyle: {
+                emphasis: {
+                    shadowBlur: 10,
+                    shadowOffsetX: 0,
+                    shadowColor: 'rgba(0, 0, 0, 0.5)'
+                }
+            }
+        }
+    ]
+};
+
+assetsOption = {
+    title: {
+        text: '资产信息统计'
+    },
+    tooltip: {
+        trigger: 'axis'
+    },
+    legend: {
+        data:['主机','联盟广告','视频广告','直接访问','搜索引擎']
+    },
+    grid: {
+        left: '3%',
+        right: '4%',
+        bottom: '3%',
+        containLabel: true
+    },
+    toolbox: {
+        feature: {
+            saveAsImage: {}
+        }
+    },
+    xAxis: {
+        type: 'category',
+        boundaryGap: false,
+        data: ['周一','周二','周三','周四','周五','周六','周日']
+    }, 
+    yAxis: {
+        type: 'value'
+    },
+    series: [
+        {
+            name:'主机',
+            type:'line',
+            stack: '总量',
+            data:[120, 132, 101, 134, 90, 230, 210]
+        },
+        {
+            name:'联盟广告',
+            type:'line',
+            stack: '总量',
+            data:[220, 182, 191, 234, 290, 330, 310]
+        },
+        {
+            name:'视频广告',
+            type:'line',
+            stack: '总量',
+            data:[150, 232, 201, 154, 190, 330, 410]
+        },
+        {
+            name:'直接访问',
+            type:'line',
+            stack: '总量',
+            data:[320, 332, 301, 334, 390, 330, 320]
+        },
+        {
+            name:'搜索引擎',
+            type:'line',
+            stack: '总量',
+            data:[820, 932, 901, 934, 1290, 1330, 1320]
+        }
+    ]
+};
+
+vulnChart.setOption(vulnOption);
+assetsChart.setOption(assetsOption);
