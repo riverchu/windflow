f174a9f6f790b71c0a4dd1c8939385e5678369a2
River Chu <churiver@protonmail.com>
2018-09-22 23:08:41 +0800

    增加静态资源路径

--- src/web/windflow/windflow/settings.py
+++ src/web/windflow/windflow/settings.py
@@ -58,7 +58,7 @@ ROOT_URLCONF = 'windflow.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR+"/templates"],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -122,4 +122,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
+STATIC_ROOT = os.path.join(BASE_DIR, 'assets')
+STATICFILES_DIRS = (
+        os.path.join(BASE_DIR, 'static'),
+)
